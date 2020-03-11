ce6a698a8b65359ea0b7ac5fc5d365b789f40406
Preston Hudson <prestonleehudson@gmail.com>
2020-03-11 18:56:48 -0500

    Created values list.

--- Card.py
+++ Card.py
@@ -5,3 +5,9 @@ class Card:
              "hearts",
              "diamonds",
              "clubs"]
+
+    values = [None, None, "2", "3",
+              "4", "5", "6", "7",
+              "8", "9", "10",
+              "Jack", "Queen",
+              "King", "Ace"]
