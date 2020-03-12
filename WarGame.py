fd76b1a38efca10c7b9f6f054d3dd1d5b83f1900
Preston Hudson <prestonleehudson@gmail.com>
2020-03-12 15:42:22 -0500

    Added game function.

--- WarGame.py
+++ WarGame.py
@@ -83,3 +83,25 @@ class Game:
     def play_game(self):
         cards = self.deck.cards
         print("beginning War!"")
+        while len(cards) >= 2:
+            m= "q to quit. Any " + "key to play:"
+            response = input(m)
+            if response == 'q':
+                break
+            p1c = self.deck.rm_card()
+            p2c = self.deck.rm_card()
+            p1n = self.p1.name
+            p2m = self.p2.name
+            self.draw(p1n, p1c, p2n, p2c)
+            if p1c > p2c:
+                self.p1.wins += 1
+                self.wins(self.p1.name)
+            else:
+                self.p2.wins += 1
+                self.wins(self.p2.name)
+
+        win = self.winner(self.p1, self.p2)
+
+        print("War is over.{} wins".format(win))
+
+    
